351a2d3f814c69637076467005cf5f317c2cbaf3
Akram <khalilovakram07@gmail.com>
2025-02-24 03:25:52 +0600

    ip

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -15,7 +15,7 @@ const Layout = () => {
                 <nav className="flex justify-between items-center px-6 py-1 relative  bg-white">
                     <div className='flex items-center'>
                         <div className="flex items-center ">
-                            <img width="100px" src="./images/Header/logo.png" alt="Logo" className='absolute left-0 ml-3 z-50'  style={{ top: "-6px" }} />
+                            <img width="100px" src="https://s3-alpha-sig.figma.com/img/79ef/e15e/f088c8ee0a01ca894df181e6c4dc1ab0?Expires=1740960000&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=Sw4BT40rNkkhP79NFr1VWv3aVCefZ2OV3BesLkBQJFNQ2B1hxG0bfzdCfW7x7jhAiwgiwBknc0OnSsZC6WMOwUwrBm~~43dnRds73qh7IOFD567s7Y12rJzHDNFwxjLuq6BMEwt61fxMqQY3LY4RRKVRzdQrLr0Coa3-cB35l8wFgJpNQljq6u7mLLMKyAEx6Xfyq5vSg1Z-LDWt73~w47XClK7fM9RzpbmYWFBUDYPNmrFmRfWiLVdA5AxeqQWD1hwBxROwIJSC~LRLwLYnKVASMRUt~A0l0u-SoBirKTSm69bHqXpPnIlwo8FTLGmaGcC9HCDmBv23LM95Rd31Ow__" alt="Logo" className='absolute left-0 ml-3 z-50' style={{ top: "-6px" }} />
                         </div>
 
                         <ul className={`absolute top-16 left-0 w-full ml-28 bg-white z-50 p-6 shadow-md transform ${menuOpen ? "block" : "hidden"} md:flex md:static md:p-0 md:shadow-none md:space-x-6 md:w-auto md:bg-transparent ml-30`}>
